c19d0066fe64e0eb12a2fee4dc55f8933cf80796
majay3574 <majay3574@gmail.com>
2024-05-18 18:30:43 +0530

    first commit

---
+++ tests/deleteLead.spec.ts
@@ -0,0 +1,23 @@
+import {getLead} from "../api_Integration/getLead"
+import { test } from "../customFixtures/salesforceFixture";
+
+// async function getLeadName(): Promise<any> {
+//     return await getLead();   
+// }
+   
+test.use({ storageState: "logins/salesforce.json" });
+
+test(`Deleting the LeadID created from an API request`, async ({ SalesforceHomePage, SalesforceLeadPage }) => {
+    const userName = await getLead()
+    await SalesforceHomePage.appLauncher();
+    await SalesforceHomePage.viewAll();
+    await SalesforceHomePage.searchApp("Leads");
+    await SalesforceHomePage.app("Leads");
+    await SalesforceLeadPage.searchLead(userName);
+    await SalesforceLeadPage.leadID(userName);
+    await SalesforceLeadPage.expandButton();
+    await SalesforceLeadPage.deleteLead();
+    await SalesforceLeadPage.deletePopUP();
+    await SalesforceLeadPage.searchLead(userName);
+    await SalesforceLeadPage.verifiTheDeletedData();
+});
