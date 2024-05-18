de8b02c3f0c09faf8e39a1efe7e9337e9acf88aa
majay3574 <majay3574@gmail.com>
2024-05-18 18:33:30 +0530

    committed

--- declartiontypes/playwright-custom.d.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from "@playwright/test";
-
-declare module '@playwright/test'{
-    interface Page{
-        delayedFill:(selector:string, value:string) => Promise<void>;
-        clickAndDelay:(selector:string) => Promise<void>;
-    }
-
-}
